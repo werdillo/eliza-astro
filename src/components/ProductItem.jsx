925556513b38c7016f1fb0ff39c4fc9869f24764
werdillo <wediktivus@gmail.com>
2024-11-16 01:12:54 +0200

    swiper and product page

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,38 +1,119 @@
 import { For, Show, createSignal, onMount } from "solid-js";
-import { client, getImage } from '../lib/pocketbase';
+import { client, getImage } from "../lib/pocketbase";
+import { register } from 'swiper/element/bundle';
+register();
 
-export default function ProductItem({type='def'}) {
-    const [item, setItem] = createSignal({});
-    const [loading, setLoading] = createSignal(true);
-	const urlSearchParams = new URLSearchParams(window.location.search);
-	const params = Object.fromEntries(urlSearchParams.entries());
-    onMount(async () => {
-		const param = params.name;
-        const collectionName = type === 'def' ? 'products' : 'mattresses'
-        try {
-            const res = await client.collection(collectionName).getList(1, 50, {
-                filter: `path="${param}"`,
-            });
-            setItem(res.items[0]);
-            setLoading(false);
-        } catch (err) {
-            console.error('Error fetching items:', err);
-        }
-    });
+export default function ProductItem({ type = "def" }) {
+  const [item, setItem] = createSignal({});
+  const [loading, setLoading] = createSignal(true);
+  const urlSearchParams = new URLSearchParams(window.location.search);
+  const params = Object.fromEntries(urlSearchParams.entries());
 
-    return (
-        <div>
-            <Show when={loading()}>
-                <div class="skeleton"></div>
+  onMount(async () => {
+    const param = params.name;
+    const collectionName = type === "def" ? "products" : "mattresses";
+
+    try {
+      const res = await client.collection(collectionName).getList(1, 50, {
+        filter: `path="${param}"`,
+      });
+      setItem(res.items[0]);
+      setLoading(false);
+    } catch (err) {
+      console.error("Error fetching items:", err);
+    }
+  });
+
+  return (
+    <div>
+      <Show when={loading()}>
+        <div class="skeleton"></div>
+      </Show>
+      <Show when={!loading()}>
+        <div class="product-item">
+          <div>
+            <Show
+              when={item().images?.length > 1}
+              fallback={
+                <img
+                  src={
+                    type === "def"
+                      ? getImage(item(), item().images?.[0])
+                      : getImage(item(), item().image)
+                  }
+                  alt=""
+                  class="-slider"
+                />
+              }
+            >
+              <swiper-container
+                slides-per-view="1"
+                navigation="true"
+                pagination="true"
+                pagination-type="fraction"
+                speed="500" 
+                loop="true" 
+                autoplay-delay="5000"
+                class="product-item-swipert"
+              >
+                <For each={item().images}>
+                  {(image) => (
+                    <swiper-slide>
+                        <img src={getImage(item(), image)} alt="" class="-slider"/>
+                    </swiper-slide>
+                    )}
+                </For>
+              </swiper-container>
             </Show>
-            <Show when={!loading()}>
-                <div class='product-item'>
-                    <div>
-                        <img src={type==='def' ? getImage(item(), item().images[0]) : getImage(item(), item().image)} alt="" class="-slider" />
-                        <p class='-text xl'>{item().name}</p>
-                    </div>
+            </div>
+                <div className="-right">
+                <div class="-title">{item().name}</div>
+                <div className="-text">
+                <b>Gulēšanas funkcija</b> - nav <br/>
+                <b>Veļas kaste</b> - nav <br/>
+                <b>Korpuss -</b> Dabīgskoks,  <br/>
+                <b>Sēde/sēdes pilveni</b> - Porolons,  <br/>
+                <b>Muguras spilveni -  </b>Sintepona vate + spalvas <br/>
+
                 </div>
-            </Show>
+                <button class="-button xl">Pievienot grozam</button>
+
+            </div>
+            
+         
+            {/* <div className="-card">
+
+            </div> */}
         </div>
-    );
+        <div class="product-item l">
+            <div className="-card">
+                <div className="-text m bd">
+                    Apraksts
+                </div>
+                <div className="-text">
+                    Atsperu matracis „Diamond De Lux” būs piemērots gulētājiem, kas pastiprināti svīst miega laikā. Kā arī tiem, kam patīk gulēt uz vidēji stingra matrača ar gaisīgi mīkstu virsmu. ClimaLatekss materiāls, ko izmantojam matrača virskārtā ir dabīgs, mīksts materiāls, kas izgatavots no kaučuka koka sulas ekstrakta. Pateicoties īpašajam sastāvam un perforācijai, matracis uzturēs regulāru gaisa cirkulāciju. Matracis ir divpusējs – abas puses vienādas.
+
+                </div>
+                <div>
+                    <button class="-button">Text</button>
+                </div>
+            </div>
+            <div className="-card">
+                <div className="-text m bd">
+                    Audumi
+                </div>
+                <div className="-textile">
+                    <img src="https://eliza.pockethost.io/api/files/6mym3bbn87vzkzf/1n3lu4328dwqq97/matrix_28Pq5LpZ1J.jpeg?token=" alt="" className="-img" />
+                    <img src="https://eliza.pockethost.io/api/files/6mym3bbn87vzkzf/1n3lu4328dwqq97/matrix_28Pq5LpZ1J.jpeg?token=" alt="" className="-img" />
+                    <img src="https://eliza.pockethost.io/api/files/6mym3bbn87vzkzf/1n3lu4328dwqq97/matrix_28Pq5LpZ1J.jpeg?token=" alt="" className="-img" />
+
+                </div>
+                <div>
+                    <button class="-button">Text</button>
+                </div>
+            </div>
+        </div>
+      </Show>
+    </div>
+  );
 }

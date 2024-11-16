5ae71bcc5e7bfa2522d8405af07e6e72673e3ae4
werdillo <wediktivus@gmail.com>
2024-11-17 01:29:52 +0200

    some updates

---
+++ src/components/PostItem.jsx
@@ -0,0 +1,49 @@
+import { createResource, Suspense } from "solid-js";
+import { client } from '../lib/pocketbase';
+import { getImageUrl } from '../lib/pocketbase';
+
+const fetchPostItem = async (name) => {
+	const res = await client.collection('blog').getList(1, 50, {
+		filter: `path="${name}"`,
+	});
+	if (res.items.length > 0) {
+		return res.items[0];
+	}
+	throw new Error("Item not found");
+};
+
+export default function PostItem() {
+	const urlSearchParams = new URLSearchParams(window.location.search);
+	const params = Object.fromEntries(urlSearchParams.entries());
+	const [item, { refetch }] = createResource(() => params.name, fetchPostItem);
+
+	return (
+		<Suspense fallback={
+			<>
+				<div class="skeleton post"></div>
+				<div class="left">
+					<div class="skeleton text title"></div>
+					<div class="skeleton text"></div>
+					<div class="skeleton text"></div>
+					<div class="skeleton text"></div>
+					<div class="skeleton text"></div>
+					<div class="skeleton text"></div>
+					<div class="skeleton text"></div>
+					<div class="skeleton text"></div>
+				</div>
+			</>
+		}>
+			{item() && (
+				<>
+					<img src={getImageUrl(item())} class="post-image" alt={item().name} />
+					<div class="container">
+						<div class="post">
+							<div class="-title">{item().title}</div>
+							<div class="-text" innerHTML={item().content}></div>
+						</div>
+					</div>
+				</>
+			)}
+		</Suspense>
+	);
+}
---
+++ src/components/Slider.jsx
@@ -0,0 +1,46 @@
+import { For, createSignal, onMount } from "solid-js";
+import { register } from "swiper/element/bundle";
+register();
+
+export default function Slider() {
+  const [items, setItems] = createSignal([
+    {
+      images: [
+        "/slider/Pic1.jpg",
+        "/slider/Pic2.jpg",
+        "/slider/Pic3.jpg",
+        "/slider/Pic4.jpg",
+        "/slider/Pic5.jpg",
+        "/slider/Pic6.jpg",
+        "/slider/Pic7.jpg",
+        "/slider/Pic8.jpg",
+      ],
+    },
+  ]);
+  return (
+    <swiper-container
+        slides-per-view="1"
+        navigation="true"
+        pagination="true"
+        speed="500"
+        loop="true"
+        autoplay-delay="5000"
+    >
+    <For each={items()[0].images}>
+        {(image) => (
+        <swiper-slide>
+            <div
+            style={{
+                "background-image": `url(${image})`,
+                "background-size": "cover",
+                "background-position": "center",
+                width: "100%",
+                height: "700px",
+            }}
+            ></div>
+        </swiper-slide>
+        )}
+    </For>
+    </swiper-container>
+  );
+}

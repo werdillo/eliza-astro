eed2f37ae65e314f3f49b72b34dcd9f8c79d8674
Alexey <alexey.tihomirov7@gmail.com>
2025-03-11 23:56:05 +0200

    translate for mobile menu

---
+++ src/components/layout/Links.js
@@ -0,0 +1,53 @@
+  export const mainLinks = [
+    { 
+      href: "/collections", 
+      label_en: "Collections",
+      label_lv: "Kolekcijas",
+      label_ru: "Коллекции"
+    },
+    { 
+      href: "/horeca", 
+      label_en: "HoReCa",
+      label_lv: "HoReCa",
+      label_ru: "HoReCa"
+    },
+    { 
+      href: "/matressess", 
+      label_en: "Mattreses",
+      label_lv: "Matrači",
+      label_ru: "Матрасы"
+    },
+    { 
+      href: "/components", 
+      label_en: "Components",
+      label_lv: "Komponenti",
+      label_ru: "Компоненты"
+    }
+  ];
+  
+  export const secondaryLinks = [
+    { 
+      href: "/gallery", 
+      label_en: "Gallery",
+      label_lv: "Galerija",
+      label_ru: "Галерея"
+    },
+    { 
+      href: "/catalogs", 
+      label_en: "Catalogs",
+      label_lv: "Katalogi",
+      label_ru: "Каталоги"
+    },
+    { 
+      href: "/about-us", 
+      label_en: "About us",
+      label_lv: "Par mums",
+      label_ru: "О нас"
+    },
+    { 
+      href: "/contact", 
+      label_en: "Contact",
+      label_lv: "Kontakti",
+      label_ru: "Контакты"
+    }
+  ];

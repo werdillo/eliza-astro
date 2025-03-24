77381a0cece1139312fa47bb9aad0e32367fabe6
Alexey Tihomirov <alexey.tihomirov7@gmail.com>
2025-03-24 16:15:31 +0200

    Footer and contacts static

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,49 +1,61 @@
 import { For, createSignal, onMount } from "solid-js";
-import { client } from '../lib/pocketbase';
+import { client } from "../lib/pocketbase";
+import { contacts, translations } from "../contacts";
 
+export default function Contacts({ lang }) {
+  const [items, setItems] = createSignal({});
+  const [loading, setLoading] = createSignal(true);
 
-export default function Contacts({lang}) {
-	const [items, setItems] = createSignal([]);
-	const [loading, setLoading] = createSignal(true);
-
-	onMount(async () => {
-		try {
+  onMount(async () => {
+    try {
 			const res = await client.collection('contacts').getList(1, 50);
 			setItems(res.items);
 			console.log(res.items)
 			setLoading(false);
 		} catch (err) {
-			console.error('Error fetching items:', err);
-		}
-	});
-	const title = {
-		en: "Working time",
-		ru: "Время работы",
-		lv: "Darba laiks"
-	};
-	return <>
-		<div class="contacts">
-			<Show when={loading()}>
-				<div class="skeleton"></div>
-				<div class="skeleton"></div>
-				<div class="skeleton"></div>
-			</Show>
-			<Show when={!loading()}>
-				<For each={items()}>
-					{(item) => (
-						<div class='contacts-wrapper'>
-							<p class='-text xl special'>{item['title_' + lang]}</p>
-							<p class='-text'>{item.address}</p>
-							<p class='-text'>{item.email}</p>
-							<p class='-text'>{item.phone}</p>
-							<p class='-text xl work-time'>{title[lang]}</p>
-							<p class='-text'>{item['weekday_' + lang]}  </p>
-							<p class='-text'>{item['break_' + lang]}    </p>
-							<p class='-text'>{item['weeken_' + lang]}  </p>
-						</div>
-					)}
-				</For>
-			</Show>
-		</div>
-	</>
+      console.error("Error fetching work times:", err);
+    }
+  });
+
+  const title = {
+    en: "Working time",
+    ru: "Время работы",
+    lv: "Darba laiks",
+  };
+
+  return (
+    <div class="contacts">
+      <For each={contacts}>
+        {(contact, index) => 
+            <div class="contacts-wrapper">
+              <p class="-text xl special">{translations[lang][contact.key]}</p>
+              <p class="-text">
+				<a href={`https://maps.google.com/?q=${contact.address}`} target="_blank" class="contact-link">
+					{contact.address}
+				</a>
+				</p>
+			  <p class="-text">
+				<a href={`mailto:${contact.mail}`}>{contact.mail}</a>
+			  </p>
+              <p class="-text">
+				{contact.phone.map(phone => (
+					<a href={`tel:+371${phone}`}>{phone}</a>
+				)).reduce((prev, curr) => prev === null ? [curr] : [prev, ', ', curr], null)}
+			  </p>
+              <p class="-text xl work-time">{title[lang]}</p>
+              <Show when={!loading()} fallback={<>
+				<div class="skeleton text title wide"></div>
+				<div class="skeleton text title wide"></div>
+				<div class="skeleton text title wide"></div>
+				</>
+				}>
+                <p class="-text">{items()[index()]["weekday_" + lang] || "—"}</p>
+                <p class="-text">{items()[index()]["break_" + lang] || "—"}</p>
+                <p class="-text">{items()[index()]["weeken_" + lang] || "—"}</p>
+              </Show>
+            </div>
+          }
+      </For>
+    </div>
+  );
 }
--- src/contacts.js
+++ src/contacts.js
@@ -1,17 +1,37 @@
 export const contacts = [
-    showRoom = { 
-      address: "Rīga, A. Deglava iela 50, LV-1035", 
-      mail: "teika@eliza-k.lv",
-      phone: "29112499",
-    },
-    foam = { 
-      address: "Rīga, A. Deglava iela 52A, LV-1035", 
-      mail: "order@eliza-k.lv",
-      phone: "27862121, 26653510",
-    },
-    textile = { 
-      address: "Rīga, A. Deglava 50 iela K-1", 
-      mail: "audumi@eliza-k.lv",
-      phone: "26599422",
-    },
-  ];
+  {
+    "key": "showroom",
+    "address": "Rīga, A. Deglava iela 50, LV-1035",
+    "mail": "teika@eliza-k.lv",
+    "phone": ["29112499"]
+  },
+  {
+    "key": "foam",
+    "address": "Rīga, A. Deglava iela 52A, LV-1035",
+    "mail": "order@eliza-k.lv",
+    "phone": ["27862121", "26653510"]
+  },
+  {
+    "key": "textile",
+    "address": "Rīga, A. Deglava 50 iela K-1",
+    "mail": "audumi@eliza-k.lv",
+    "phone": ["26599422"]
+  }
+]
+export const translations = {
+  lv: {
+    showroom: "Mēbeļu salons ELIZA",
+    foam: "Porolona detaļas ražošana",
+    textile: "Audumu noliktava"
+  },
+  en: {
+    showroom: "Furniture showroom ELIZA",
+    foam: "Foam details manufacture",
+    textile: "Textile warehouse"
+  },
+  ru: {
+    showroom: "Мебельный салон ELIZA",
+    foam: "Производство деталей из поролона",
+    textile: "Склад тканей"
+  }
+};

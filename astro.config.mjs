f5fa41f53d56aa9b33efa39ec60fbbb24de52309
Alexey Tihomirov <alexey.tihomirov7@gmail.com>
2025-09-13 20:31:55 +0300

    small seo optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,25 @@
-import { defineConfig } from 'astro/config';
-
-import solidJs from '@astrojs/solid-js';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs()]
+  site: "https://eliza-k.lv",
+  integrations: [
+    solidJs(),
+    sitemap({
+      i18n: {
+        defaultLocale: "lv",
+        locales: {
+          lv: "lv",
+          en: "en",
+          ru: "ru",
+        },
+      },
+      filter: (page) => !page.includes("/admin") && !page.includes("/test"),
+      changefreq: "weekly",
+      priority: 0.7,
+      lastmod: new Date(),
+    }),
+  ],
 });

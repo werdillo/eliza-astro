a68f82d37c4b5ef60aa5a12a208e67d1e06afe61
Alexey Tihomirov <alexey.tihomirov7@gmail.com>
2025-09-14 00:10:14 +0300

    fixed collection page

--- src/components/Collection.jsx
+++ src/components/Collection.jsx
@@ -1,4 +1,4 @@
-import { For, createSignal, onMount } from "solid-js";
+import { For, Show, createSignal, onMount } from "solid-js";
 import { client, getImage } from "../lib/pocketbase";
 
 export default function Collections({ brand, lang }) {
@@ -32,51 +32,48 @@ export default function Collections({ brand, lang }) {
     }
   });
 
-  if (loading()) {
-    return (
-      <div>
-        <div class="container">
-          <div class="skeleton text title" />
-        </div>
-        <div class="product-list">
-          {Array(8)
-            .fill(0)
-            .map(() => (
-              <div class="skeleton" />
-            ))}
-        </div>
+  const SkeletonLoader = () => (
+    <>
+      <div class="container">
+        <div class="skeleton text title"></div>
       </div>
-    );
-  }
+      <div class="product-list">
+        {Array(8)
+          .fill()
+          .map(() => (
+            <div class="skeleton" />
+          ))}
+      </div>
+    </>
+  );
 
   return (
-    <For each={Object.entries(groupedItems())}>
-      {([type, items]) => (
-        <div>
-          <div class="container">
-            <div class="title">
-              {translate[lang]?.[type.toLowerCase()] || type}
+    <Show when={!loading()} fallback={<SkeletonLoader />}>
+      <For each={Object.entries(groupedItems())}>
+        {([type, items]) => (
+          <>
+            <div class="container">
+              <div class="title">{translate[lang][type.toLowerCase()]}</div>
             </div>
-          </div>
-          <div class="product-list">
-            <For each={items}>
-              {(item) => (
-                <a href={`/${lang}/product?name=${item.path}`}>
-                  <div class="product-landing">
-                    <img
-                      src={getImage(item, item.images?.[0])}
-                      alt={item.name}
-                      class="-img"
-                      loading="lazy"
-                    />
-                    <p class="-text">{item.name.toUpperCase()}</p>
-                  </div>
-                </a>
-              )}
-            </For>
-          </div>
-        </div>
-      )}
-    </For>
+            <div class="product-list">
+              <For each={items}>
+                {(item) => (
+                  <a href={`/${lang}/product?name=${item.path}`}>
+                    <div class="product-landing">
+                      <img
+                        src={getImage(item, item.images[0])}
+                        alt=""
+                        class="-img"
+                      />
+                      <p class="-text">{item.name.toUpperCase()}</p>
+                    </div>
+                  </a>
+                )}
+              </For>
+            </div>
+          </>
+        )}
+      </For>
+    </Show>
   );
 }
